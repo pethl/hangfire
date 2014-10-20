8c34112ee4f7650de7931cc44b7ba77f59b1411f
pethl <admin@fencedweller.com>
2014-10-20 23:34:01 +0100

    baseproduct averages

--- app/models/baseproduct.rb
+++ app/models/baseproduct.rb
@@ -1,3 +1,33 @@
 class Baseproduct < ActiveRecord::Base
   belongs_to :ingredient
+  before_save :calc_prices
+  
+  def price_per
+    if self.total_price.blank? || self.total_weight.blank?
+      return "unavailable"
+    else
+   
+    self.price_per = self.total_price.to_f/self.total_weight.to_f
+  end
+  end
+  
+  def calc_prices
+        if self.total_weight.blank? 
+              if !self.unit_weight.blank? && !self.unit_count.blank? 
+                self.total_weight = self.unit_weight * self.unit_count
+        end
+      end
+        if self.unit_price.blank? 
+               if !self.total_price.blank? && !self.unit_count.blank? 
+                 Rails.logger.debug("in test: #{self.total_price.inspect}")
+                 self.unit_price = self.total_price / self.unit_count
+         end
+    end
+        if self.total_price.blank? || self.total_weight.blank?
+          return "unavailable"
+        else
+
+        self.price_per = self.total_price.to_f/self.total_weight.to_f
+      end
+  end
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,7 +1,6 @@
 class Product < ActiveRecord::Base
   has_many :items
   
-    MEAT_TYPES = ["--", "Beef", "Chicken", "Duck", "Pork", "Turkey"]
     
     def self.get_name(id)
       name = Product.where(:id => id)[0].name
