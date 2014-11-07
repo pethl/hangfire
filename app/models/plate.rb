8640a615b00df29a302966859836e6603d799506
pethl <admin@fencedweller.com>
2014-11-07 12:12:23 +0000

    plates index bug

--- app/models/plate.rb
+++ app/models/plate.rb
@@ -1,6 +1,7 @@
 class Plate < ActiveRecord::Base
   has_many :productlinks
   
+  
     accepts_nested_attributes_for :productlinks, allow_destroy: true
       validates :category_id, presence: true
             validates :price, presence: true
