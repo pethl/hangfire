98037d27e4e747250183e75ede400aa02bf116e8
pethl <pethicklisa@gmail.com>
2017-05-16 14:36:55 +0100

    sign in redirect and remove faqs

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,7 +7,7 @@ class SessionsController < ApplicationController
    user = User.find_by(email: params[:session][:email].downcase)
      if user && user.authenticate(params[:session][:password])
          log_in user
-            redirect_to orders_summary_orders_path
+            redirect_to static_pages_dashboard_path
        else
 
         # flash.now[:danger] = 'Invalid email/password combination'
